6d2e338b62f0d8e68a90a4186a732b21501c9300
Sreelakshmi Suresh <30370485+Sreelakshmisuresh97@users.noreply.github.com>
2026-07-28 11:08:47 -0400

    Update: longest-repeating-substring-with-replacement - submission-1

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -1,17 +1,34 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
 
         l = 0
-        maxf = 0
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
 
-            while (r - l + 1) - maxf > k:
-                count[s[l]] -= 1
-                l += 1
-            res = max(res, r - l + 1)
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
 
-        return res
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
